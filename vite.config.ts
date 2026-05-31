f58ecc2794f540e1493c068c79b44ad35fde50a1
kafelllll <nastasencik@gmail.com>
2026-05-31 23:03:09 +0300

    refactor: simplify base URL configuration in production environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const base =
-    env.VITE_APP_BASE_URL ?? (mode === 'production' ? '/bachelor_senchyk_app/' : '/');
+  const base = env.VITE_APP_BASE_URL ?? '/';
 
   return {
     base,
